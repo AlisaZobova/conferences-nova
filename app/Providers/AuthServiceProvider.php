00443aba14512a4b6b477db027a05f61899babad
Alisa Zobova <zobovaalice@gmail.com>
2023-03-13 18:44:30 +0200

    feat: add zoom meetings and comments policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Comment;
+use App\Models\ZoomMeeting;
+use App\Policies\CommentPolicy;
+use App\Policies\ZoomMeetingPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -15,6 +19,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Comment::class => CommentPolicy::class,
+        ZoomMeeting::class => ZoomMeetingPolicy::class,
     ];
 
     /**
@@ -28,10 +34,11 @@ class AuthServiceProvider extends ServiceProvider
 
         // Implicitly grant "Super Admin" role all permissions
         // This works in the app by using gate-related functions like auth()->user->can() and @can()
-        Gate::before(
-            function ($user, $ability) {
-                return $user->hasRole('Admin') ? true : null;
-            }
-        );
+
+//        Gate::before(
+//            function ($user, $ability) {
+//                return $user->hasRole('Admin') ? true : null;
+//            }
+//        );
     }
 }
