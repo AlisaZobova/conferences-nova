451c682b85ff32afb0b044293f82012ea09e986c
Alisa Zobova <zobovaalice@gmail.com>
2023-03-16 13:33:45 +0200

    feat: update user resource fields

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -4,11 +4,13 @@ namespace App\Nova;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Date;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Zobova\PhoneInput\PhoneInput;
 
 class User extends Resource
 {
@@ -54,13 +56,25 @@ class User extends Resource
         return [
             ID::make()->sortable(),
 
-            Gravatar::make()->maxWidth(50),
-
-            Text::make('Firstname')
+            Text::make('First Name', 'firstname')
                 ->rules('required', 'max:255'),
 
-            Text::make('Lastname')
+            Text::make('Last Name', 'lastname')
                 ->rules('required', 'max:255'),
+
+            Date::make('Birthdate')
+                ->rules('required', 'before_or_equal:' . now()),
+
+            Text::make('Phone')
+                ->rules('required', 'max:255')->exceptOnForms(),
+
+            PhoneInput::make('Phone')->onlyOnForms()->rules('required'),
+
+            Text::make('Country', function () {
+                return $this->country ? $this->country->name : '';
+            })->exceptOnForms(),
+
+            BelongsTo::make('Country')->rules('required')->onlyOnForms(),
 
             Text::make('Email')
                 ->rules('required', 'email', 'max:254')
