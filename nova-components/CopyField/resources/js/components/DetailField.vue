a9c32f2b4a0c38f2075c2f8ed53ff9ec2239ca4a
Alisa Zobova <zobovaalice@gmail.com>
2023-03-17 08:51:36 +0200

    feat: add the possibility of unsecured copying in the field with zoom links for the http protocol

--- nova-components/CopyField/resources/js/components/DetailField.vue
+++ nova-components/CopyField/resources/js/components/DetailField.vue
@@ -13,9 +13,26 @@
 export default {
     props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
     methods: {
-      copyFieldValue() {
-          navigator.clipboard.writeText(this.field.value)
-      },
+        unsecuredCopyToClipboard() {
+            const textArea = document.createElement("textarea");
+            textArea.value = this.field.value;
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                console.error('Unable to copy to clipboard', err);
+            }
+            document.body.removeChild(textArea);
+        },
+        copyFieldValue() {
+            if (window.isSecureContext && navigator.clipboard) {
+                navigator.clipboard.writeText(this.field.value);
+            } else {
+                this.unsecuredCopyToClipboard();
+            }
+        },
     }
 }
 </script>
