9525e6b76c45664b07e5ed1a36020ed220cd1ee9
Alisa Zobova <zobovaalice@gmail.com>
2023-03-23 08:24:10 +0200

    feat: add report topic to search array

--- app/Nova/Report.php
+++ app/Nova/Report.php
@@ -38,7 +38,7 @@ class Report extends Resource
      * @var array
      */
     public static $search = [
-        'id',
+        'id', 'topic'
     ];
 
     public static $with = ['user', 'conference', 'meeting', 'category'];
