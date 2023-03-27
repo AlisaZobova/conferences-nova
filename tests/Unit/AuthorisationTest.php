f844064448318d3f3eb1783988694d258ae2a49d
Alisa Zobova <zobovaalice@gmail.com>
2023-03-27 12:49:08 +0300

    feat: add tests for authorisation

---
+++ tests/Unit/AuthorisationTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class AuthorisationTest extends TestCase
+{
+    public function test_successful_auth() {
+        $user = User::factory()->create();
+
+        $response = $this->json('POST', 'api/login',
+            ['email' => $user['email'], 'password' => '12345678']);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_fail_auth() {
+        $user = User::factory()->create();
+
+        $response = $this->json('POST', 'api/login',
+            ['email' => $user['email'], 'password' => '11111111']);
+
+        $response->assertStatus(422);
+
+        $response->assertInvalid(["email" => "These credentials do not match our records."]);
+    }
+
+    public function test_fail_admin_auth() {
+        $response = $this->json('POST', 'api/login',
+            ['email' => 'admin@example.com', 'password' => '12345678']);
+
+        $response->assertStatus(422);
+
+        $response->assertInvalid(["email" => "These credentials do not match our records."]);
+    }
+
+    public function test_fail_if_auth() {
+        $user = User::factory()->create();
+
+        $newUser = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->json('POST', 'api/login', ['email' => $newUser['email'], 'password' => '12345678']);
+
+        $response->assertStatus(500);
+    }
+}
